67cb6d62a3645aa8774385f4fc131773067885d2
徐家俊 <15151832830@163.com>
2024-12-09 14:31:41 +0800

    init deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    modules: [
+      '@pinia/nuxt',
+      '@unocss/nuxt',
+      '@vueuse/nuxt',
+      '@nuxthub/core',
+    ],
+    devtools: { enabled: true, vscode: {} },
 })
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: 徐家俊 15151832830@163.com
+ * @Date: 2024-12-02 11:11:06
+ * @LastEditTime: 2024-12-02 12:39:03
+ * @LastEditors: 徐家俊 15151832830@163.com
+ * @Description: 
+ * @FilePath: /my-nuxt/pages/index.vue
+-->
+<script setup lang="ts">
+
+    const route = useRoute()
+    const counter = ref(0)
+
+</script>
+
+<template>
+    <div>
+        Counter: {{ counter }}
+        <button @click="counter++">+</button>
+        <button @click="counter--">-</button>
+    </div>
+    <div>
+        <h1>Nuxt Routing set up successfully!</h1>
+        <p>Current route: {{ route.path }}</p>
+        <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    </div>
+</template>
